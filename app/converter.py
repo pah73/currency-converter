b693f0732a21739fdadf5dc88ff8fc327478204c
Paul Hadchiti <pah73@georgetown.edu>
2021-05-09 16:12:57 -0400

    restrarting

--- app/converter.py
+++ app/converter.py
@@ -1,6 +1,6 @@
 # Python program to convert the currency
 # of one country to that of another country 
-  
+
 # Import the modules needed
 import os
 import json
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from app import APP_ENV
 
 load_dotenv()
-  
+
 
 # source for code below: https://www.geeksforgeeks.org/currency-converter-in-python/
 
@@ -20,24 +20,24 @@ class Currency_convertor:
     rates = {} 
     def __init__(self, url):
         data = requests.get(url).json()
-  
+
         # Extracting only the rates from the json data
         self.rates = data["rates"] 
-  
+
     # function to do a simple cross multiplication between 
     # the amount and the conversion rates
     def convert(self, from_currency, to_currency, amount):
         initial_amount = amount
         if from_currency != 'EUR' :
             amount = amount / self.rates[from_currency]
-  
+
         # limiting the precision to 2 decimal places
         amount = round(amount * self.rates[to_currency], 2)
         print('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency))
-    
+
 # Driver code
 if __name__ == "__main__":
-  
+
     # API key is stored in .env file
     CURRENCY_API_KEY = "ed8098eb0481545d8a4822cdd2b09f08"
     url = str.__add__('http://data.fixer.io/api/latest?access_key=', CURRENCY_API_KEY)  
@@ -45,5 +45,5 @@ if __name__ == "__main__":
     from_country = input("From Country: ")
     to_country = input("TO Country: ")
     amount = int(input("Amount: "))
-  
+
     c.convert(from_country, to_country, amount)
