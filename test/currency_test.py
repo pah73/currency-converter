fb0c8451e8b73cc94c5f497ee188be615cdf0bea
Paul Hadchiti <pah73@georgetown.edu>
2021-05-17 12:16:37 -0400

    working on tests

--- test/currency_test.py
+++ test/currency_test.py
@@ -1,26 +1,28 @@
 import os
 import pytest
 
-
 from app.convertor import currency_convertor, exchange_rates
 
 CI_ENV = os.getenv("CI") == "true"
 
 @pytest.mark.skipif(CI_ENV==True, reason="to avoid issuing HTTP requests on the CI server")
 
-def test_currency_convertor():
-#     # with valid geography, returns the city name and forecast info:
-#     results = get_hourly_forecasts(country_code="US", zip_code="20057")
-#     assert results["city_name"] == "Washington, DC"
-#     assert len(results["hourly_forecasts"]) == 24
-#     forecast = results["hourly_forecasts"][0]
-#     assert sorted(list(forecast.keys())) == ["conditions", "image_url", "temp", "timestamp"]
-#     assert forecast["timestamp"].endswith(":00")
-#     assert f"{DEGREE_SIGN}F" in forecast["temp"]
 
-#     # with invalid geography, fails gracefully and returns nothing:
-    invalid_results = currency_convertor(currency_from="USD",currency_to="OOOPS",amount=10)
+def test_symbol():
+    invalid_results = currency_convertor(currency_from = "USD", currency_to = "FAIL", amount = 15)
     assert invalid_results == None
+   # rate = "AED"
+   # parsed_response = get_response(symbol)
+
+    #assert isinstance(parsed_response, dict)
+  #  assert parsed_response[]
+
+
+
+    
+
+
+
 
 # def test_hour_formatting():
 #     assert format_hour("2021-03-29T21:00:00-04:00") == "21:00"
